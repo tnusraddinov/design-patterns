1d96da25fa3bc0e68eb6552cc6197bde56faacff
Tamerlan N <tamerlan5@hotmail.com>
2022-12-11 23:20:53 +0300

    builder pattern 2 added

--- java/src/main/java/org/example/creational/builder_002/Meal.java
+++ java/src/main/java/org/example/creational/builder_002/Meal.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Meal {
-    private List<Item> items = new ArrayList<Item>();
 
-    public void addItem(Item item){
+    private final List<Item> items = new ArrayList<Item>();
+
+    public void addItem(Item item) {
         items.add(item);
     }
 
-    public float getCost(){
+    public float getCost() {
         float cost = 0.0f;
-
-        for (Item item : items) {
-            cost += item.price();
-        }
-        return cost;
+        return items.stream()
+                .map(Item::price)
+                .reduce(Float::sum)
+                .orElse(cost);
     }
 
-    public void showItems(){
+    public void showItems() {
 
         for (Item item : items) {
             System.out.print("Item : " + item.name());
